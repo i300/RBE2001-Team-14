413c42e274f54e9ae06465afac2acbb53df41602
Augusto Rolando Wong <augustowong2010@hotmail.com>
2016-10-08 14:50:45 -0400

    Update DriveTrain.cpp

--- src/Subsystems/DriveTrain/DriveTrain.cpp
+++ src/Subsystems/DriveTrain/DriveTrain.cpp
@@ -31,7 +31,7 @@ DriveTrain::DriveTrain(int leftMotorPin, int rightMotorPin, int alignmentSwitchP
   }
 }
 
-/* writeToMotors
+/* writeToMotors - void
  *
  * left - Left motor speed
  * right - Light motor speed
@@ -41,7 +41,8 @@ void DriveTrain::writeToMotors(float left, float right) {
   rightMotor->write(right);
 }
 
-/* arcadeDrive - Drives robot based on a forward speed and rotation value
+/* arcadeDrive - void 
+ * Drives robot based on a forward speed and rotation value
  *
  * speed - Speed robot will drive at
  * rotation - Value from -1 (left) to 1 (right)
@@ -79,7 +80,8 @@ void DriveTrain::arcadeDrive(float speed, float rotation) {
   writeToMotors(leftMotorSpeed, rightMotorSpeed);
 }
 
-/* tankDrive - Drive robot based on left values and right speeds
+/* tankDrive - void
+ * Drive robot based on left values and right speeds
  *
  * left - Left speed
  * right - Right speed
@@ -93,7 +95,8 @@ void DriveTrain::tankDrive(float left, float right) {
   writeToMotors(left, right);
 }
 
-/* followLine - Drive the robot by following a line using PID control
+/* followLine - double 
+ * Drive the robot by following a line using PID control
  *
  * speed - speed to drive forward
  * @returns PID loop result
@@ -126,8 +129,12 @@ double DriveTrain::followLine(float speed) {
   return rotation;
 }
 
-/*
+/* turnOntoLine - bool8 
+ * Turns the robot towards a new line
  *
+ * speed - speed to drive
+ * direction - direction of turning
+ * @returns true or false depending if the robot achieved the task
  */
 bool8 DriveTrain::turnOntoLine(float speed, int direction) {
   direction = constrain(direction, -1, 1);
@@ -150,8 +157,10 @@ bool8 DriveTrain::turnOntoLine(float speed, int direction) {
     return false;
   }
 }
-/* updateLineCount - int horizontal lines
+/* updateLineCount - int 
  * keeps track of how many horizontal lines the robot has traveled
+ *
+ * @returns the number of horizontal lines traveled
  */
 int DriveTrain::updateLineCount() {
   unsigned char numSensors = _lineSensor->getNumSensors();
@@ -177,7 +186,7 @@ void DriveTrain::resetLineCount() {
 }
 
 /* isAlignmentSwitchPressed - bool8 
- * returns 0 or 1 depending if the switch has been pressed
+ * @returns 0 or 1 depending if the switch has been pressed
  */
 bool8 DriveTrain::isAlignmentSwitchPressed() {
   Serial.println(digitalRead(_alignmentSwitchPin));
@@ -185,7 +194,7 @@ bool8 DriveTrain::isAlignmentSwitchPressed() {
 }
 
 /* stop - void
- *Sends zero to the drive motors to stop motion
+ * Sends zero to the drive motors to stop motion
  */
 void DriveTrain::stop() {
   writeToMotors(0, 0);
